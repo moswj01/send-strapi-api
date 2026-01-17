6c3504a47a3dcaa3bfa9de3018014d99ed0267e5
moswj0 <mosv.chara@gmail.com>
2026-01-17 23:03:44 +0700

    OAuth

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,8 +1,17 @@
-export default () => ({
+export default ({ env }) => ({
   'users-permissions': {
     config: {
       register: {
         allowedFields: ['username', 'email', 'password', 'p_no', 'img_cover', 'credit'],
+      },
+      providers: {
+        google: {
+          enabled: true,
+          clientId: env('GOOGLE_CLIENT_ID'),
+          clientSecret: env('GOOGLE_CLIENT_SECRET'),
+          // ใช้ custom scheme ตรงกับ Google OAuth Client
+          callback: 'sendapp://auth/callback',
+        },
       },
     },
   },
--- dist/config/plugins.js
+++ dist/config/plugins.js
@@ -1,10 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = () => ({
+exports.default = ({ env }) => ({
     'users-permissions': {
         config: {
             register: {
                 allowedFields: ['username', 'email', 'password', 'p_no', 'img_cover', 'credit'],
+            },
+            providers: {
+                google: {
+                    enabled: true,
+                    clientId: env('GOOGLE_CLIENT_ID'),
+                    clientSecret: env('GOOGLE_CLIENT_SECRET'),
+                    // ใช้ custom scheme ตรงกับ Google OAuth Client
+                    callback: 'sendapp://auth/callback',
+                },
             },
         },
     },
